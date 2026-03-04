fa82242901aa04f70aa1c5a56a75da8cd8eba352
yaneekumsudsang <zx.0937575349@gmail.com>
2026-03-04 16:08:41 +0700

    update Ui Chicken dark mode

--- dashboard/components/sensors_right.php
+++ dashboard/components/sensors_right.php
@@ -1,35 +1,35 @@
-<div id="card-right-1" class="bg-white rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
-    <div id="card-right-1-title" class="text-[0.5vw] font-semibold text-slate-600">Offline</div>
+<div id="card-right-1" class="bg-white dark:bg-stone-900 rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
+    <div id="card-right-1-title" class="text-[0.5vw] font-semibold text-slate-600 dark:text-white">Offline</div>
     <div class="text-center">
         <span id="card-right-1-value" class="text-[0.65vw] font-bold text-slate-800">--</span>
     </div>
 </div>
-<div id="card-right-2" class="bg-white rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
-    <div id="card-right-2-title" class="text-[0.5vw] font-semibold text-slate-600">Offline</div>
+<div id="card-right-2" class="bg-white dark:bg-stone-900 rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
+    <div id="card-right-2-title" class="text-[0.5vw] font-semibold text-slate-600 dark:text-white">Offline</div>
     <div class="text-center">
         <span id="card-right-2-value" class="text-[0.65vw] font-bold text-slate-800">--</span>
     </div>
 </div>
-<div id="card-right-3" class="bg-white rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
-    <div id="card-right-3-title" class="text-[0.5vw] font-semibold text-slate-600">Offline</div>
+<div id="card-right-3" class="bg-white dark:bg-stone-900 rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
+    <div id="card-right-3-title" class="text-[0.5vw] font-semibold text-slate-600 dark:text-white">Offline</div>
     <div class="text-center">
         <span id="card-right-3-value" class="text-[0.65vw] font-bold text-slate-800">--</span>
     </div>
 </div>
-<div id="card-right-4" class="bg-white rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
-    <div id="card-right-4-title" class="text-[0.5vw] font-semibold text-slate-600">Offline</div>
+<div id="card-right-4" class="bg-white dark:bg-stone-900 rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
+    <div id="card-right-4-title" class="text-[0.5vw] font-semibold text-slate-600 dark:text-white">Offline</div>
     <div class="text-center">
         <span id="card-right-4-value" class="text-[0.65vw] font-bold text-slate-800">--</span>
     </div>
 </div>
-<div id="card-right-5" class="bg-white rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
-    <div id="card-right-5-title" class="text-[0.5vw] font-semibold text-slate-600">Offline</div>
+<div id="card-right-5" class="bg-white dark:bg-stone-900 rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
+    <div id="card-right-5-title" class="text-[0.5vw] font-semibold text-slate-600 dark:text-white">Offline</div>
     <div class="text-center">
         <span id="card-right-5-value" class="text-[0.65vw] font-bold text-slate-800">--</span>
     </div>
 </div>
-<div id="card-right-6" class="bg-white rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
-    <div id="card-right-6-title" class="text-[0.5vw] font-semibold text-slate-600">Offline</div>
+<div id="card-right-6" class="bg-white dark:bg-stone-900 rounded-xl border border-stone-400 shadow-sm p-[0.25vw] min-w-[100px]">
+    <div id="card-right-6-title" class="text-[0.5vw] font-semibold text-slate-600 dark:text-white">Offline</div>
     <div class="text-center">
         <span id="card-right-6-value" class="text-[0.65vw] font-bold text-slate-800">--</span>
     </div>
